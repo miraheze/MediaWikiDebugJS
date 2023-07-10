99ea1e84efa047a3587ca9542a0fe9994b950fc6
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-07-10 13:19:19 -0600

    Remove usage of mediawiki

--- content_script.js
+++ content_script.js
@@ -1,4 +1,18 @@
-( function ( $, mw ) {
+// Equivalent to `mw.config.get('variable')`. We have to scrape the value
+// from the script source because Chrome extension content scripts do not share
+// an execution environment with other JavaScript code.
+function getMediaWikiVariable(variable) {
+	var nodes = document.querySelectorAll('script'),
+		i, match;
+
+	for (i = 0; i < nodes.length; i++) {
+		match = new RegExp('"' + variable + '":\\s*"?([^"]+)"?').exec(nodes[i].innerText);
+		if (match) {
+			return match[1].replace(',', '');
+		}
+	}
+}
+
 function parseHttpHeaders(httpHeaders) {
 	return httpHeaders.split("\n").map(function (x) {
 		return x.split(/: */, 2);
@@ -18,41 +32,46 @@ function checkHtmlBody() {
 
 		xhr.onload = function () {
 			const headers = parseHttpHeaders(xhr.getAllResponseHeaders()),
-				respTime = mw.config.get('wgBackendResponseTime'),
+				respTime = getMediaWikiVariable('wgBackendResponseTime'),
 				backend = 'PHP7',
-				server = mw.config.get('wgHostname').replace('.wikiforge.net', ''),
+				server = getMediaWikiVariable('wgHostname').replace('.wikiforge.net', ''),
 				cp = (headers['x-served-by'] ? headers['x-served-by'] : '').replace(/.wikiforge.net|^mw[0-9]+|^test[0-9]+|\s|,/g, ''),
-				dbname = mw.config.get('wgDBname'),
+				dbname = getMediaWikiVariable('wgDBname') || 'unknown',
 				info = respTime.toString() + 'ms (<b>' + backend + '</b> via ' + dbname + '@' + server + (cp ? ' / ' + cp : '') + ')';
 
-			if (mw.config.get('skin') === 'cosmos') {
+			const skin = document.body.className.match(/skin-([a-z]+)/);
+			const skinName = skin ? skin[1] : '';
+
+			if (skinName === 'cosmos') {
 				$('<li>').html(info).appendTo('#p-tb ul');
 			} else {
 				$('<li>').html(info).prependTo('#p-personal ul');
 			}
 
-			$.when(mw.loader.using(["mediawiki.api"])).then(function () {
-				return new mw.Api()
-					.get({
-						action: "query",
-						meta: "siteinfo",
-						siprop: "statistics",
-						format: "json",
-					})
-					.then(function (data) {
-						const jobs = data.query.statistics.jobs,
-							caption = 'Queued Jobs: ' + jobs;
+			const apiUrl = '/w/api.php';
+			const params = {
+				action: 'query',
+				meta: 'siteinfo',
+				siprop: 'statistics',
+				format: 'json',
+			};
 
-						if (mw.config.get('skin') === 'cosmos') {
-							$('<li>').html(caption).appendTo('#p-tb ul');
-						} else {
-							$('<li>').html(caption).prependTo('#p-personal ul');
-						}
-					});
-			});
-		}
+			fetch(apiUrl + '?' + new URLSearchParams(params))
+				.then(function (response) {
+					return response.json();
+				})
+				.then(function (data) {
+					const jobs = data.query.statistics.jobs,
+						caption = 'Queued Jobs: ' + jobs;
+
+					if (skinName === 'cosmos') {
+						$('<li>').html(caption).appendTo('#p-tb ul');
+					} else {
+						$('<li>').html(caption).prependTo('#p-personal ul');
+					}
+				});
+		};
 	}
 }
 
 checkHtmlBody();
-}( jQuery, mediaWiki ) );
