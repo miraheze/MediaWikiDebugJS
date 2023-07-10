c123133817c775b81f7b79bb4e1cf34c0fee7075
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-07-10 17:01:32 -0600

    Use constants to define skins and wiki farms

--- content_script.js
+++ content_script.js
@@ -1,3 +1,20 @@
+// In the format of: 'hostname': 'selector'
+const WIKI_FARMS = {
+	// Covers both WikiForge and WikiTide
+	'wikiforge.net': 'static.wikiforge.net',
+	// Add more wiki farms here as needed
+};
+
+// In the format of: skinname: 'selector'
+const SKIN_SELECTORS = {
+	cosmos: '#p-tb ul',
+	minerva: 'ul#p-personal',
+	// Add more skin selectors here as needed
+};
+
+const cache = {};
+const cacheDuration = 300000; // 5 minutes
+
 // Equivalent to `mw.config.get('variable')`. We have to scrape the value
 // from the script source because Chrome extension content scripts do not share
 // an execution environment with other JavaScript code.
@@ -24,94 +41,100 @@ function parseHttpHeaders(httpHeaders) {
 	}, {});
 }
 
-const cache = {};
-
-// 5 minutes
-const cacheDuration = 300000;
-
 function checkHtmlBody() {
-	if (document.body.innerHTML.includes("wikiforge")) {
-		const xhr = new XMLHttpRequest();
-		xhr.open('HEAD', document.location);
-		xhr.send();
+	const bodyContent = document.body.innerHTML;
 
-		xhr.onload = function () {
-			const headers = parseHttpHeaders(xhr.getAllResponseHeaders()),
-				respTime = getMediaWikiVariable('wgBackendResponseTime'),
-				backend = 'PHP7',
-				server = getMediaWikiVariable('wgHostname').replace('.wikiforge.net', ''),
-				cp = (headers['x-served-by'] ? headers['x-served-by'] : '').replace(/.wikiforge.net|^mw[0-9]+|^test[0-9]+|\s|,/g, ''),
-				dbname = getMediaWikiVariable('wgDBname') || 'unknown',
-				info = respTime.toString() + 'ms (<b>' + backend + '</b> via ' + dbname + '@' + server + (cp ? ' / ' + cp : '') + ')';
+	const includesAnyOf = (string, substrings) => {
+		return substrings.some((substring) => string.includes(substring));
+	};
 
-			const skin = document.body.className.match(/skin-([a-z]+)/);
-			const skinName = skin ? skin[1] : '';
+	const matchingWikiFarms = Object.entries(WIKI_FARMS).filter(([domain]) => {
+		return includesAnyOf(bodyContent, [domain]);
+	});
 
-			if (skinName === 'cosmos') {
-				const liInfoElement = document.createElement('li');
-				liInfoElement.innerHTML = info;
-				document.querySelector('#p-tb ul').appendChild(liInfoElement);
-			} else if (skinName === 'minerva') {
-				const liInfoElement = document.createElement('li');
-				liInfoElement.innerHTML = info;
-				document.querySelector('ul#p-personal').appendChild(liInfoElement);
-			} else {
-				const liInfoElement = document.createElement('li');
-				liInfoElement.innerHTML = info;
-				document.querySelector('#p-personal ul').appendChild(liInfoElement);
-			}
-
-			const apiUrl = '/w/api.php';
-			const params = {
-				action: 'query',
-				meta: 'siteinfo',
-				siprop: 'statistics',
-				format: 'json',
-			};
-
-			// Check if the API response is cached and not expired
-			if (cache.hasOwnProperty(apiUrl) && (Date.now() - cache[apiUrl].timestamp) < cacheDuration) {
-				handleApiResponse(cache[apiUrl].data);
-			} else {
-				// Fetch the API response
-				fetch(apiUrl + '?' + new URLSearchParams(params))
-					.then(function (response) {
-						return response.json();
-					})
-					.then(function (data) {
-						// Cache the API response with the timestamp
-						cache[apiUrl] = {
-							data: data,
-							timestamp: Date.now()
-						};
-
-						handleApiResponse(data);
-					});
-			}
-		};
+	if (matchingWikiFarms.length === 0) {
+		return; // No matching wiki farms found in the HTML body
 	}
+
+	const xhr = new XMLHttpRequest();
+	xhr.open('HEAD', document.location);
+	xhr.send();
+
+	xhr.onload = function () {
+		const headers = parseHttpHeaders(xhr.getAllResponseHeaders()),
+			respTime = getMediaWikiVariable('wgBackendResponseTime'),
+			backend = 'PHP7',
+			server = getMediaWikiVariable('wgHostname'),
+			cp = (headers['x-served-by'] ? headers['x-served-by'] : '').replace(/.wikiforge.net|^mw[0-9]+|^test[0-9]+|\s|,/g, ''),
+			dbname = getMediaWikiVariable('wgDBname') || 'unknown',
+			info = respTime.toString() + 'ms (<b>' + backend + '</b> via ' + dbname + '@' + server + (cp ? ' / ' + cp : '') + ')';
+
+		const skin = document.body.className.match(/skin-([a-z]+)/);
+		const skinName = skin ? skin[1] : '';
+
+		const matchingSkinSelectors = matchingWikiFarms.reduce((selectors, [domain, selector]) => {
+			const wikiFarmSelector = selector ? `.${selector}` : '';
+			const skinSelector = SKIN_SELECTORS[skinName + wikiFarmSelector];
+			if (skinSelector) {
+				selectors.push(skinSelector);
+			}
+			return selectors;
+		}, []);
+
+		if (matchingSkinSelectors.length === 0) {
+			return; // No matching skin selectors found for the wiki farms
+		}
+
+		const liInfoElement = document.createElement('li');
+		liInfoElement.innerHTML = info;
+
+		matchingSkinSelectors.forEach((skinSelector) => {
+			const targetElement = document.querySelector(skinSelector);
+			if (targetElement) {
+				targetElement.appendChild(liInfoElement.cloneNode(true));
+			}
+		});
+
+		const apiUrl = '/w/api.php';
+		const params = {
+			action: 'query',
+			meta: 'siteinfo',
+			siprop: 'statistics',
+			format: 'json',
+		};
+
+		if (cache.hasOwnProperty(apiUrl) && (Date.now() - cache[apiUrl].timestamp) < cacheDuration) {
+			handleApiResponse(cache[apiUrl].data, matchingSkinSelectors);
+		} else {
+			fetch(apiUrl + '?' + new URLSearchParams(params))
+				.then(function (response) {
+					return response.json();
+				})
+				.then(function (data) {
+					cache[apiUrl] = {
+						data: data,
+						timestamp: Date.now(),
+					};
+
+					handleApiResponse(data, matchingSkinSelectors);
+				});
+		}
+	};
 }
 
-function handleApiResponse(data) {
-	const jobs = data.query.statistics.jobs,
-		caption = 'Queued Jobs: ' + jobs;
+function handleApiResponse(data, matchingSkinSelectors) {
+	const jobs = data.query.statistics.jobs;
+	const caption = 'Queued Jobs: ' + jobs;
 
-	const skin = document.body.className.match(/skin-([a-z]+)/);
-	const skinName = skin ? skin[1] : '';
+	const liJobsElement = document.createElement('li');
+	liJobsElement.innerHTML = caption;
 
-	if (skinName === 'cosmos') {
-		const liJobsElement = document.createElement('li');
-		liJobsElement.innerHTML = caption;
-		document.querySelector('#p-tb ul').appendChild(liJobsElement);
-	}  else if (skinName === 'minerva') {
-		const liJobsElement = document.createElement('li');
-		liJobsElement.innerHTML = caption;
-		document.querySelector('ul#p-personal').appendChild(liJobsElement);
-	} else {
-		const liJobsElement = document.createElement('li');
-		liJobsElement.innerHTML = caption;
-		document.querySelector('#p-personal ul').appendChild(liJobsElement);
-	}
+	matchingSkinSelectors.forEach((skinSelector) => {
+		const targetElement = document.querySelector(skinSelector);
+		if (targetElement) {
+			targetElement.appendChild(liJobsElement.cloneNode(true));
+		}
+	});
 }
 
 checkHtmlBody();
