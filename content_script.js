f9ea4e12fa01d0828194a5a716d192746d0ee005
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-07-10 11:54:30 -0600

    Add content_script.js

---
+++ content_script.js
@@ -0,0 +1,9 @@
+function checkHtmlBody() {
+	if (document.body.innerHTML.includes("wikiforge")) {
+		const script = document.createElement('script');
+		script.src = '//meta.wikiforge.net/wiki/User:Universal_Omega/common.js?action=raw&ctype=text/javascript';
+		document.head.appendChild(script);
+	}
+}
+
+document.addEventListener('DOMContentLoaded', checkHtmlBody);
