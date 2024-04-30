fc5846ddaa964b6b3418aa27478dfa2af366e6de
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-04-30 15:58:09 -0600

    Simply service worker

--- background.js
+++ background.js
@@ -1,31 +1,10 @@
 chrome.runtime.onInstalled.addListener(() => {
-	chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-		if (changeInfo.status === "complete" && tab.url) {
-			chrome.scripting.executeScript({
-				target: { tabId },
-				function: injectScript,
-				args: [{ tabId }]
-			});
-		}
-	});
+    chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+        if (changeInfo.status === "complete" && tab.url) {
+            chrome.scripting.executeScript({
+                target: { tabId },
+                files: ["content_script.js"]
+            });
+        }
+    });
 });
-
-const injectScript = ({ tabId }) => {
-	chrome.tabs.get(tabId, (tab) => {
-		if (tab && tab.url) {
-			chrome.scripting.executeScript({
-				target: { tabId },
-				function: checkHtmlHead,
-				args: [{ tabUrl: tab.url }]
-			});
-		}
-	});
-};
-
-const checkHtmlHead = ({ tabUrl }) => {
-	chrome.scripting.executeScript({
-		target: { tabId: tabId },
-		function: injectScript,
-		args: [{ tabUrl }]
-	});
-};
